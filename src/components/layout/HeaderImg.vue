65c629924760531d37cce6cf10d846c6fbf20191
sunzhipeng123321 <szp961113@163.com>
2020-05-16 16:56:16 +0800

    修改昵称，修改密码

--- src/components/layout/HeaderImg.vue
+++ src/components/layout/HeaderImg.vue
@@ -5,24 +5,48 @@
             <img :src='HeaderImg' alt="">
         </div>
         <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>修改头像</el-dropdown-item>
-            <el-dropdown-item>修改密码</el-dropdown-item>
-            <el-dropdown-item>修改昵称</el-dropdown-item>
-            <el-dropdown-item>
-                <div @click="onMyEssayClick">我的文章</div>
-            </el-dropdown-item>
+            <el-dropdown-item><div @click="onChangePwd">修改密码</div></el-dropdown-item>
+            <el-dropdown-item><div @click="onChangNickClick">修改昵称</div></el-dropdown-item>
+            <el-dropdown-item><div @click="onMyEssayClick">我的文章</div></el-dropdown-item>
         </el-dropdown-menu>
+        <ChangeNickname v-if="isChangNickName" @cloneChangeNickModal='cloneChangeNickModal'/>
+        <ChangePassWord v-if="isChangePwd"  @cloneChangePwdModal='cloneChangePwdModal'/>
     </el-dropdown>
+    
 </template>
 <script>
 import HeaderImg from '@/assets/Header.jpg'
+import ChangeNickname from './../user/ChangeNickname'
+import ChangePassWord from './../user/ChangePassWord'
 export default {
     data(){
         return {
             HeaderImg,
+            isChangNickName :false,
+            isChangePwd : false,
         }
     },
-    methods: {
+        
+    created(){
+        
+    },
+    components: {
+        ChangeNickname,
+        ChangePassWord
+    },
+    methods : {
+        onChangNickClick(){
+            this.isChangNickName = true;
+        },
+        cloneChangeNickModal() {
+            this.isChangNickName = false;
+        },
+        onChangePwd(){
+            this.isChangePwd = true;
+        },
+        cloneChangePwdModal(){
+            this.isChangePwd = false;
+        },
         onMyEssayClick() {
             this.$router.push({name:'myList'});
         }
---
+++ src/components/user/ChangeNickname.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-dialog title="修改昵称" :visible='true' width="30%" :before-close="handleClose">
+        <el-input  placeholder="请输入新昵称" v-model="nickName"></el-input>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="$emit('cloneChangeNickModal')">取 消</el-button>
+            <el-button type="primary" @click="submit">确 定</el-button>
+        </span>
+    </el-dialog>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            nickName : '',
+        };
+    },
+    methods: {
+        submit() {
+            this.$emit('cloneChangeNickModal');
+        },
+        handleClose() {
+            this.$emit('cloneChangeNickModal');
+        }
+    }
+  };
+</script>
+<style scoped>
+</style>
